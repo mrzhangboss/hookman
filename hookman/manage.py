2182a3b64921370abfad7d1837affd663a055698
mrzhangboss <2529450174@qq.com>
2016-08-14 14:50:41 +0800

    finish run stop test

--- hookman/manage.py
+++ hookman/manage.py
@@ -1,8 +1,11 @@
 # -*- coding:utf-8 -*-
 from __future__ import absolute_import, unicode_literals, print_function
+import os
 import subprocess
 import multiprocessing
 from hookman.weblistener import  app
+PID_FILE = '/home/zhanglun/hookman.pid'
+ERROR_LOG = '/home/zhanglun/hookman.log'
 
 def main():
     import sys
@@ -12,16 +15,30 @@ def main():
     parse = argparse.ArgumentParser()
     # todo: complete help text
     parse.add_argument('--version', action='version', version='0.1.0')
-    parse.add_argument('--stop')
+    parse.add_argument('--stop', action='store_true')
     parse.add_argument('--run', action='store_true')
     parse.add_argument('--restart')
     args = parse.parse_args()
     # todo: analyze args and start work
     if args.run == True:
-    #     pass
-        r = subprocess.call(['python', '/home/zhanglun/hookman/hookman/weblistener.py', '&'],
-                            shell=True)
-    import time
-    time.sleep(2)
-    # app.run(host='0.0.0.0', port=3610)
-    print('listen :3610, hookman run!!!')
+        error_file = open(ERROR_LOG, mode='a+')
+        pid_file = open(PID_FILE, mode='w')
+        args = {'args': ['python', '/home/zhanglun/hookman/hookman/weblistener.py', '&']}
+        popen = subprocess.Popen(['python /home/zhanglun/hookman/hookman/weblistener.py'],
+                                 shell=True,
+                                 stderr=error_file,
+                                 stdout=error_file,
+                                 preexec_fn=os.setsid)
+        pid_file.write(str(popen.pid))
+        pid_file.close()
+        print('listen :3610, hookman run!!!')
+    elif args.stop == True:
+        if os.path.exists(PID_FILE):
+            print('stop hookman!!!')
+            os.remove(PID_FILE)
+        else:
+            print('hookman not running!!!')
+
+
+
+
--- tests/test_function.py
+++ tests/test_function.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import, unicode_literals, print_function
 import unittest
 import subprocess
 
-# todo: test is complex
 class RuningTest(unittest.TestCase):
     def test_run(self):
         self.maxDiff = None
@@ -40,40 +39,25 @@ class RuningTest(unittest.TestCase):
         result = subprocess.getoutput('curl -X POST --header "X-GitHub-Event: push" http://localhost:3610/')
         self.assertIn('hookman-0.1.0 get', result)
 
-from hookman.weblistener import app
-from flask import Request
-class AppHookmanTest(unittest.TestCase):
+class RunStopTest(unittest.TestCase):
     def setUp(self):
-        self.client = app.test_client()
-
-    def tearDown(self):
-        pass
-
-
-    def test_hookman_allow_get(self):
-        res = self.client.get('/')
-        self.assertEqual(res.status_code, 405)
-
-    def test_hookman_allow_post(self):
-        res = self.client.post('/')
-        self.assertEqual(res.status_code, 200)
-
-    def test_hookman_can_ping_back(self):
-        header = {'X-GitHub-Event': 'ping'}
-        res = self.client.post('/', headers=header)
-        self.assertEqual(res.data, b'pong')
-
-    def test_hookman_can_get_push_command(self):
-        header = {'X-GitHub-Event': 'push'}
-        res = self.client.post('/', headers=header)
-        self.assertEqual(res.data, b'hookman-0.1.0 get')
+        # stop it first
+        subprocess.getoutput('hookman --stop')
 
 
 
+    def test_run_then_stop(self):
+        start_run = subprocess.getoutput('hookman --run')
 
+        self.assertEqual('listen :3610, hookman run!!!', start_run)
 
+        stop_run = subprocess.getoutput('hookman --stop')
+        self.assertEqual('stop hookman!!!', stop_run)
 
-
+    def test_stop_twice(self):
+        stop_run = subprocess.getoutput('hookman --stop')
+        stop_run = subprocess.getoutput('hookman --stop')
+        self.assertEqual('hookman not running!!!', stop_run)
 
 if __name__ == '__main__':
     unittest.main()
